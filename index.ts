e457ebb0b6abd006dcce441cdc4ed127a7236da3
Eddi3_As <AQ43820@etsmtl.net>
2022-11-05 19:18:12 -0400

    parametrized app

--- index.ts
+++ index.ts
@@ -1,32 +1,47 @@
 import { verifyTemp } from "./utils/tempcal";
+import { verifyArgs } from "./utils/argsvalidator";
 
 import signalR = require("@microsoft/signalr");
 import fetch = require('node-fetch');
 const AC_ON = "lower";
 const HEATER_ON = "higher";
+const base_url = 'http://159.203.50.71/';
+const api = 'api/Hvac/';
+let token = 'NQ1JVDCqdg';
+let temp_max = 5;
+let temp_min = 1;
+let ticks = 6;
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://159.203.50.71/SensorHub?token=NQ1JVDCqdg")
+const args = process.argv; // [token] [limite chaud] [limite froid] [ticks]
+
+let newData = verifyArgs(args);
+
+if(newData.length === 1){
+  throw Error('ERROR TOKEN UNDEFINED');
+}
+
+token = newData[2];
+temp_max = newData[3];
+temp_min = newData[4];
+ticks = newData[5];
+
+let connection = new signalR.HubConnectionBuilder()
+    .withUrl(base_url + "SensorHub?token=" + token)
     .build();
 
 connection.on("ReceiveSensorData", (data: any) => {
     console.log(data);
-    if(verifyTemp(data, 5.00, 1.00) === HEATER_ON){
-      fetch("http://159.203.50.71/api/Hvac/NQ1JVDCqdg/TurnOnHeater/6")
+    if(verifyTemp(data, temp_max, temp_min) === HEATER_ON){
+      fetch(base_url + api + token +"/TurnOnHeater/" + ticks)
       .then((result: { json: () => any; }) => result.json())
       .then((textformat: any) => console.log(textformat))
     }
-    else if(verifyTemp(data, 5.00, 1.00) === AC_ON){
-      fetch("http://159.203.50.71/api/Hvac/NQ1JVDCqdg/TurnOnAc/6")
+    else if(verifyTemp(data, temp_max, temp_min) === AC_ON){
+      fetch(base_url + api+ token + "/TurnOnAc/" + ticks)
       .then((result: { json: () => any; }) => result.json())
       .then((textformat: any) => console.log(textformat))
     }
-});/*
-To start the AC of the unit : GET {serverUrl}/api/Hvac/{token}/TurnOnAc/{nbTicks}
-To start the Heater of the unit : GET {serverUrl}/api/Hvac/{token}/TurnOnHeater/{nbTicks}
-*/
-
-
+});
 
 const port = Number.parseInt(process.env.PORT || '3000');
 if (Number.isNaN(port)) {
@@ -34,5 +49,5 @@ if (Number.isNaN(port)) {
   process.exit(1);
 }
 
-  
 connection.start();
+
---
+++ utils/argsvalidator.ts
@@ -0,0 +1,29 @@
+export function verifyArgs(data: any[]){
+    let total = data.length;// [token] [limite chaud] [limite froid] [ticks]
+    if(total < 3){
+        return ["ERROR TOKEN UNDEFINED"];
+    }
+    else if(data[2].length <= 1){
+        return ["ERROR TOKEN UNDEFINED"];
+    }else if(total < 4){
+        console.log("MISSING [limite chaud] [limite froid] [ticks]");
+        let newData = data;
+        newData[3] = 5;
+        newData[4] = 1;
+        newData[5] = 6; //default  [limite chaud] [limite froid] [ticks]
+        return newData;
+    }
+    else if(total < 5){
+        console.log("MISSING [limite froid] [ticks]");
+        let newData = data;
+        newData[4] = 1;
+        newData[5] = 6; //default [limite froid] [ticks]
+        return newData;
+    }else if(total < 6){
+        console.log("MISSING [ticks]");
+        let newData = data;
+        newData[5] = 6; //default [ticks]
+        return newData;
+    }
+    return data;
+}
